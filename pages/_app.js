86a4b56a8cbef10c6585306ae16df99ef2fbb1ba
Santylx <102611029+Santylx@users.noreply.github.com>
2025-07-19 05:13:45 -0400

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 // pages/_app.js
-import '@/styles/globals.css';
-import Header from '@/components/Header';
+import '../styles/globals.css';
+import Header from '../components/Header';
 import { useRouter } from 'next/router';
 
 export default function App({ Component, pageProps }) {
